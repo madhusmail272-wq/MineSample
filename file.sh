24c37e59be4750adc0b447385578e56660586d14
Sripriya1802 <tatipamulasripriya1802@gmail.com>
2025-10-13 10:44:25 +0530

    added file.sh

---
+++ file.sh
@@ -0,0 +1,12 @@
+#this is file to check given number are postive 
+
+echo Enter a number
+read num
+if [ $num -ge 0 ]
+then
+	echo entered number is positive
+else
+	echo entered number is negative
+
+fi
+
